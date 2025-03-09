631fc8e4885daf260c893ccc29f3026432ae5786
aLoneMass <mapcer@gmail.com>
2025-03-09 22:33:58 +0300

    authentication over service account

--- bot/google_sheets.py
+++ bot/google_sheets.py
@@ -1,28 +1,26 @@
 import gspread
+import json
 from google.oauth2.service_account import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 
 # Файл с учетными данными OAuth
-CREDENTIALS_FILE = "/opt/telegram_bots/hairdresser_sophia/client_secret_722540990069-rj20hk64r3tqqel1nldea0b659689na6.apps.googleusercontent.com.json"
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
-def authenticate_google():
-    """
-    Авторизация через OAuth 2.0 и сохранение токена.
-    """
-    flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-    creds = flow.run_console()  # Запрашивает код в терминале
-    return creds
+CREDENTIALS_FILE = "/opt/telegram_bots/hairdresser_sophia/credentials.json"
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly",
+          "https://spreadsheets.google.com/feeds",
+          "https://www.googleapis.com/auth/drive"]
+SHEET_ID = "1zMoBFXK8lhPOMWreRSRDGGIaotOYerRP8QpyxiCE-z0" # Замените на ваш Spreadsheet ID
 
 def get_services():
     """
     Читает список услуг из Google Таблицы.
     """
-    creds = authenticate_google()
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds =  Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=scope)
     client = gspread.authorize(creds)
 
-    # Замените на ваш Spreadsheet ID
-    SHEET_ID = "1zMoBFXK8lhPOMWreRSRDGGIaotOYerRP8QpyxiCE-z0"
+    # Открываем таблицу по ID
     sheet = client.open_by_key(SHEET_ID).sheet1  # Открываем первый лист
 
     data = sheet.get_all_records()  # Читаем данные
