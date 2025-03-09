d839cbd42a1c5436986c3a4023e45811e767da65
aLoneMass <mapcer@gmail.com>
2025-03-09 17:50:40 +0300

    5

--- bot/ai_assistant.py
+++ bot/ai_assistant.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_ORG_ID = os.getenv("OPENAI_ORG_ID")
 
-openai_client = openai.OpenAI(api_key=OPENAI_API_KEY)
+openai_client = openai.OpenAI(
+    api_key=OPENAI_API_KEY,
+    organization=OPENAI_ORG_ID
+    )
 
 def get_ai_response(user_message):
     response = openai_client.chat.completions.create(
